5c4391369614f4845cec6524951c5eab71a1ab95
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-01-07 09:53:47 -0300

    feat: create model "job"

---
+++ models/Job.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const database = require('../database/connection');
+
+const Job = database.define
+(
+    'job',
+    {
+        title: 
+        {
+            type: Sequelize.STRING,
+        },
+        description: 
+        {
+            type: Sequelize.STRING,
+        },
+        salary: 
+        {
+            type: Sequelize.STRING,
+        },
+        company: 
+        {
+            type: Sequelize.STRING,
+        },
+        email: 
+        {
+            type: Sequelize.STRING,
+        },
+        new_job: 
+        {
+            type: Sequelize.INTEGER,
+        }
+    }
+)
+
+modeule.exports = Job;
